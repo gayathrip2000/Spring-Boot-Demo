670cf0c2099a847d523f040238f8a51f3b19927b
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-12-19 01:33:08 +0530

    Second Commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "Simple-Root";
+    }
+
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
+    }
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "user Updated!";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser() {
+        return "User Deleted!";
+    }
+}
